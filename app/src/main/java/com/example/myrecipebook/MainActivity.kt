1a0185e4d4563fbf2d0bdd6894b61ea154a11751
sercel23 <ercelserap@gmail.com>
2022-02-17 01:41:02 +0300

    Add Menu

--- app/src/main/java/com/example/myrecipebook/MainActivity.kt
+++ app/src/main/java/com/example/myrecipebook/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myrecipebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.myrecipebook.databinding.ActivityMainBinding
 import com.example.myrecipebook.databinding.ActivityRecipeBinding
 
@@ -14,5 +17,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //inflater
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.recipe_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_recipe_item) {
+            val intent = Intent(this@MainActivity, RecipeActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
