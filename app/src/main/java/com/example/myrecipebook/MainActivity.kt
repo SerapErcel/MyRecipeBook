649eb00992d5700f1efa7029b7269280f0edc0ba
sercel23 <ercelserap@gmail.com>
2022-02-16 20:33:46 +0300

    Add MainActivityBinding

--- app/src/main/java/com/example/myrecipebook/MainActivity.kt
+++ app/src/main/java/com/example/myrecipebook/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.myrecipebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myrecipebook.databinding.ActivityMainBinding
+import com.example.myrecipebook.databinding.ActivityRecipeBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
